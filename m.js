4c34cc6e5293124aea96e9a66568cca5b25ed9e6
hashninja <92805325+hashninja69@users.noreply.github.com>
2022-03-23 22:52:32 +0530

    Add files via upload

---
+++ m.js
@@ -0,0 +1,11 @@
+function toggle() {
+    var x = document.getElementById("nav-items");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+console.log("com");
